4b55efe9ef5d226d8dad3841e70114082a917d36
Rakha112 <rakhawibowo1998@gmail.com>
2023-12-18 06:53:51 +0700

    Finish ShopUI3D

--- src/25-React-Native-3D-Shop/ShopUI3DScreen.tsx
+++ src/25-React-Native-3D-Shop/ShopUI3DScreen.tsx
@@ -33,7 +33,7 @@ const ShopUI3DScreen = () => {
   const dataColors = ['pink', 'blue', 'orange', 'red', 'white'];
   const rotate = useSharedValue(0);
   const position = useSharedValue(0);
-  const context = useSharedValue(0);
+
   const handleChangeDirection = () => {
     if (direction === 'y') {
       setDirection('x');
@@ -45,11 +45,8 @@ const ShopUI3DScreen = () => {
   };
   // Using new Gesture API
   const pan = Gesture.Pan()
-    .onBegin(() => {
-      context.value = position.value;
-    })
     .onUpdate(e => {
-      position.value = context.value + e.translationX;
+      position.value = e.translationX;
     })
     .onEnd(() => {
       position.value = withSpring(0);
@@ -63,35 +60,60 @@ const ShopUI3DScreen = () => {
     <GestureHandlerRootView style={{flex: 1}}>
       <SafeAreaView style={styles.container}>
         <Header handleChangeDirection={handleChangeDirection} rotate={rotate} />
-        <View style={styles.modelContainer}>
-          {loading && <Loader />}
-          <Canvas camera={{fov: 20}}>
-            <directionalLight position={[1, 0, 0]} args={['white', 2]} />
-            <directionalLight position={[-1, 0, 0]} args={['white', 2]} />
-            <directionalLight position={[0, 0, 1]} args={['white', 2]} />
-            <directionalLight position={[0, 0, -1]} args={['white', 2]} />
-            <directionalLight position={[0, 1, 0]} args={['white', 2]} />
-            <directionalLight position={[0, -1, 0]} args={['white', 2]} />
-            <Suspense fallback={<Trigger setLoading={setLoading} />}>
-              <Shoe
-                position={position}
-                direction={direction}
-                baseColor={baseColor}
-                soleColor={soleColor}
-              />
-            </Suspense>
-          </Canvas>
-        </View>
-        <GestureDetector gesture={pan}>
-          <Animated.View style={[styles.sliderContainer, animatedStyle]}>
-            <Image
-              source={require('./src/assets/Slider.png')}
-              style={styles.slider}
-            />
-          </Animated.View>
-        </GestureDetector>
         <ScrollView>
+          <View style={styles.modelContainer}>
+            {loading && <Loader />}
+            <Canvas camera={{fov: 20}}>
+              <directionalLight position={[1, 0, 0]} args={['white', 2]} />
+              <directionalLight position={[-1, 0, 0]} args={['white', 2]} />
+              <directionalLight position={[0, 0, 1]} args={['white', 2]} />
+              <directionalLight position={[0, 0, -1]} args={['white', 2]} />
+              <directionalLight position={[0, 1, 0]} args={['white', 2]} />
+              <directionalLight position={[0, -1, 0]} args={['white', 2]} />
+              <Suspense fallback={<Trigger setLoading={setLoading} />}>
+                <Shoe
+                  position={position}
+                  direction={direction}
+                  baseColor={baseColor}
+                  soleColor={soleColor}
+                />
+              </Suspense>
+            </Canvas>
+          </View>
+          <GestureDetector gesture={pan}>
+            <Animated.View style={[styles.sliderContainer, animatedStyle]}>
+              <Image
+                source={require('./src/assets/Slider.png')}
+                style={styles.slider}
+              />
+            </Animated.View>
+          </GestureDetector>
+
           <View style={styles.contentContainer}>
+            <Text style={styles.textName}>Sneakers Shoes</Text>
+            <View style={styles.ratingContainer}>
+              <Text style={styles.textRating}>(5.0)</Text>
+              <Image
+                style={styles.star}
+                source={require('./src/assets/Star.png')}
+              />
+              <Image
+                style={styles.star}
+                source={require('./src/assets/Star.png')}
+              />
+              <Image
+                style={styles.star}
+                source={require('./src/assets/Star.png')}
+              />
+              <Image
+                style={styles.star}
+                source={require('./src/assets/Star.png')}
+              />
+              <Image
+                style={styles.star}
+                source={require('./src/assets/Star.png')}
+              />
+            </View>
             <Text style={styles.textTitle}>Base Color</Text>
             <View style={styles.colorsContainer}>
               {dataColors.map((item, index) => {
@@ -120,8 +142,29 @@ const ShopUI3DScreen = () => {
                 );
               })}
             </View>
+            <Text style={styles.textTitle}>Description</Text>
+            <Text style={styles.textDescription}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse iaculis nulla tortor, at viverra augue venenatis eget.
+              Nam magna ligula, consequat sit amet purus eu, eleifend maximus
+              elit. Aliquam rhoncus fringilla venenatis. In pulvinar lacus quis
+              urna suscipit, placerat commodo turpis molestie. Cras tristique
+              suscipit nisl, eu porttitor ex.,
+            </Text>
           </View>
         </ScrollView>
+        <View style={styles.bottomContainer}>
+          <View style={styles.priceContainer}>
+            <Text style={styles.textPrice}>$199</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              console.log('BUY NOW');
+            }}>
+            <Text style={styles.textButton}>Buy Now</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
@@ -144,15 +187,67 @@ const styles = StyleSheet.create({
   contentContainer: {
     paddingHorizontal: 20,
   },
-  textTitle: {
+  textName: {
     color: 'white',
-    fontSize: 32,
+    fontSize: 34,
     fontWeight: 'bold',
     marginVertical: 10,
+  },
+  textTitle: {
+    color: 'white',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  ratingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textRating: {
+    color: 'white',
+    fontSize: 20,
+    marginRight: 10,
+  },
+  star: {
+    width: 24,
+    height: 24,
+    marginRight: 5,
   },
   colorsContainer: {
     flexDirection: 'row',
     gap: 20,
   },
-  color: {width: 50, height: 50},
+  color: {width: 40, height: 40, borderRadius: 50},
+  textDescription: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'justify',
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    padding: 10,
+  },
+  priceContainer: {
+    flex: 1 / 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textPrice: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  button: {
+    flex: 2 / 3,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    padding: 14,
+  },
+  textButton: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
 });
