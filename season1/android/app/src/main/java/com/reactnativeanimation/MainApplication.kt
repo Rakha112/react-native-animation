dbaff60102da70b2ec937eb2fbd3edab2b537c3a
Rakha112 <rakhawibowo1998@gmail.com>
2024-09-14 16:19:20 +0700

    chore(season1): upgrade React Naative to 0.75.3 and  update dependencies

--- season1/android/app/src/main/java/com/reactnativeanimation/MainApplication.kt
+++ season1/android/app/src/main/java/com/reactnativeanimation/MainApplication.kt
@@ -12,7 +12,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
@@ -34,7 +33,7 @@ class MainApplication : Application(), ReactApplication {
       })
 
   override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
+    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
@@ -43,7 +42,6 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
